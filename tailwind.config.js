c06d987c64e10e14ff331a2e4301dcb0e99c8ba4
Leonardo Garcia <leonardo.garcia@millpar.com>
2025-08-22 09:25:42 -0300

    new project

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./*.html", "./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        roboto: ["Roboto", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
